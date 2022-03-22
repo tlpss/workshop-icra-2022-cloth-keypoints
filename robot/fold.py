d8f76e63350d931bf735e67142692f8c2cb86762
tlips <thomas17.lips@gmail.com>
2022-03-22 19:31:43 +0100

    format

--- robot/fold.py
+++ robot/fold.py
@@ -1,13 +1,13 @@
-import time
-from  rtde_control import RTDEControlInterface as RTDEControl
 import numpy as np
 import scipy.spatial.transform as transform
+from rtde_control import RTDEControlInterface as RTDEControl
+
 
 def transformation_matrix_from_position_and_vecs(pos, x, y, z):
     transformation_matrix = np.eye(4)
-    transformation_matrix[:3,0] = x
-    transformation_matrix[:3,1] = y
-    transformation_matrix[:3,2] = z
+    transformation_matrix[:3, 0] = x
+    transformation_matrix[:3, 1] = y
+    transformation_matrix[:3, 2] = z
     transformation_matrix[:3, 3] = pos
     return transformation_matrix
 
@@ -52,17 +52,17 @@ class TowelFold:
 
     @staticmethod
     def pose_to_rotvec_waypoint(pose):
-        position = pose[:3,3]
-        rpy = transform.Rotation.from_matrix(pose[:3,:3]).as_rotvec()
+        position = pose[:3, 3]
+        rpy = transform.Rotation.from_matrix(pose[:3, :3]).as_rotvec()
         return np.concatenate((position, rpy))
-
-
 
 
 if __name__ == "__main__":
 
     """test setup for fold sequence."""
-    cloth = TowelFold(np.array([-.1,-0.2,0]),np.array([0.3,-0.2,0]), np.array([0.3,-0.3,0]),np.array([-0.1,-0.3,0]))
+    cloth = TowelFold(
+        np.array([-0.1, -0.2, 0]), np.array([0.3, -0.2, 0]), np.array([0.3, -0.3, 0]), np.array([-0.1, -0.3, 0])
+    )
     print(cloth.pregrasp_pose_in_cloth_frame(0.0))
     print()
     print(cloth.robot_to_cloth_base_transform)
@@ -73,29 +73,33 @@ if __name__ == "__main__":
     pregrasp_rpy = cloth.pose_to_rotvec_waypoint(pregrasp)
 
     from robotiq2f_tcp import Robotiq2F85TCP
+
     vel = 0.3
     acc = 0.2
     blend = 0.01
     wps = []
     num_waypoints = 50
-    for t in range(0,num_waypoints):
+    for t in range(0, num_waypoints):
         i = t / num_waypoints
-        wp = cloth.pose_to_rotvec_waypoint(cloth.robot_to_cloth_base_transform @cloth.fold_pose_in_cloth_frame(i))
-        wps.append(wp.tolist() + [vel, acc,blend])
+        wp = cloth.pose_to_rotvec_waypoint(cloth.robot_to_cloth_base_transform @ cloth.fold_pose_in_cloth_frame(i))
+        wps.append(wp.tolist() + [vel, acc, blend])
 
-    
     print("external control")
     rtde_c = RTDEControl("10.42.0.162")
 
     gripper = Robotiq2F85TCP("10.42.0.162")
     gripper.activate_gripper()
-    gripper.move_to_position(20,250,10)
+    gripper.move_to_position(20, 250, 10)
 
-    rtde_c.moveL(pregrasp_rpy,vel, acc)
-    check =  input("continue fold? Press Enter")
-    rtde_c.moveL(cloth.pose_to_rotvec_waypoint(cloth.robot_to_cloth_base_transform @cloth.fold_pose_in_cloth_frame(0)),vel,acc)
-    gripper.move_to_position(230,255,10)
+    rtde_c.moveL(pregrasp_rpy, vel, acc)
+    check = input("continue fold? Press Enter")
+    rtde_c.moveL(
+        cloth.pose_to_rotvec_waypoint(cloth.robot_to_cloth_base_transform @ cloth.fold_pose_in_cloth_frame(0)),
+        vel,
+        acc,
+    )
+    gripper.move_to_position(230, 255, 10)
     rtde_c.moveL(wps)
 
-    gripper.move_to_position(20,255,10)
+    gripper.move_to_position(20, 255, 10)
     gripper.close()
--- robot/marker_pose_to_pickle.py
+++ robot/marker_pose_to_pickle.py
@@ -1,21 +1,22 @@
-from camera_toolkit.zed2i import Zed2i
-from camera_toolkit.aruco_pose import get_aruco_marker_poses
+import pickle
+
 import cv2
-import pickle 
+from camera_toolkit.aruco_pose import get_aruco_marker_poses
+from camera_toolkit.zed2i import Zed2i
 
 Zed2i.list_camera_serial_numbers()
 zed = Zed2i()
-img= zed.get_mono_rgb_image()
+img = zed.get_mono_rgb_image()
 img = zed.image_shape_torch_to_opencv(img)
-cam_matrix =  zed.get_mono_camera_matrix()
+cam_matrix = zed.get_mono_camera_matrix()
 print(img.shape)
 
-img, t, r = get_aruco_marker_poses(img, cam_matrix, 0.106,cv2.aruco.DICT_6X6_250, True)
+img, t, r = get_aruco_marker_poses(img, cam_matrix, 0.106, cv2.aruco.DICT_6X6_250, True)
 print(t)
 print(r)
-cv2.imshow(",",img)
+cv2.imshow(",", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-with open("marker.pickle","wb" ) as f:
-    pickle.dump([t[0],r[0]],f)
+with open("marker.pickle", "wb") as f:
+    pickle.dump([t[0], r[0]], f)
 zed.close()
